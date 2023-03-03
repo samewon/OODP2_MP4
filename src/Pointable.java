a1ffe9687cae5e12c7d7a28ffb28d23a849e1c7b
Olov Lagerqvist <immortalbeats@gmail.com>
2023-03-03 15:59:05 +0100

    Initial commit

---
+++ src/Pointable.java
@@ -0,0 +1,10 @@
+
+public interface Pointable {
+
+    public void pointerDown(Point point);
+
+    public void pointerUp(Point point);
+
+    public void pointerMoved(Point point, boolean pointerDown);
+
+}
